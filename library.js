f369d878c71c5c3f4e32909723b2a12b9832f885
Anh Nguyen <121279445+4nhus@users.noreply.github.com>
2023-10-18 16:25:11 +1100

    Add function to display books in library

--- library.js
+++ library.js
@@ -10,3 +10,34 @@ function Book(author, title, numPages, hasBeenRead) {
 function addBookToLibrary(book) {
     mylibrary.push(book);
 }
+
+function createBookDiv(book) {
+    const bookDiv = document.createElement('div');
+    bookDiv.classList.add('book');
+
+    const bookTitle = document.createElement('h1');
+    bookTitle.textContent = book.title;
+    const bookAuthor = document.createElement('h2');
+    bookAuthor.textContent = book.author;
+    const bookNumPages = document.createElement('p');
+    bookNumPages.textContent = book.numPages;
+    const bookHasBeenRead = document.createElement('p');
+    bookHasBeenRead.textContent = book.hasBeenRead;
+
+    bookDiv.appendChild(bookTitle);
+    bookDiv.appendChild(bookAuthor);
+    bookDiv.appendChild(bookNumPages);
+    bookDiv.appendChild(bookHasBeenRead);
+
+    return bookDiv;
+}
+
+function displayBooks() {
+    const library = document.querySelector('#library');
+
+    mylibrary.forEach(book => {
+        library.appendChild(createBookDiv(book));
+    })
+}
+
+displayBooks();
